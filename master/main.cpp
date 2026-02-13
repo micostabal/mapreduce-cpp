270b39ca7ff529e73f5cbcc69b2503aacb7efb81
Martín <micostabalc@gmail.com>
2026-02-13 18:44:17 -0300

    add split words example

--- master/main.cpp
+++ master/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <memory>
 #include <grpcpp/grpcpp.h>
+#include <master.h>
 #include "master_service.h"
 #include <common/utils/string_utils.h>
 
@@ -8,7 +11,11 @@ using grpc::Server;
 using grpc::ServerBuilder;
 
 int main() {
+    
     std::cout << "MapReduce Master" << std::endl;
+    
+    std::vector<std::string> words = common::utils::string_utils::split_words("One, two, three, four, five");
+    std::cout << "Word count: " << words.size() << std::endl;
     
     const std::string address = "0.0.0.0:50051";
 
