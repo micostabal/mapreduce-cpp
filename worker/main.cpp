10cd7047b70142a6f3bc9dc1694582a13b27bf21
Martín <micostabalc@gmail.com>
2026-02-15 11:21:51 -0300

    heartbeat emitter for worker

--- worker/main.cpp
+++ worker/main.cpp
@@ -3,16 +3,27 @@
 #include <vector>
 #include <memory>
 
+#include <thread>
+#include <chrono>
 #include <grpcpp/grpcpp.h>
 
 #include "master_client.h"
 #include <common/utils/string_utils.h>
 
 
+void heartbeat_thread(mapreduce::worker::MasterClient &client) {
+    std::cout << "Starting to emit heartbeats every 1 second" << std::endl;
+    while (1) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        bool ok = client.SendHeartbeat(1);
+
+        std::cout << "Heartbeat result: " << ok << std::endl;
+    }
+}
+
 int main() {
-    
-    std::vector<std::string> words = common::utils::string_utils::split_words("Jesus is King!");
-    std::cout << "Word count: " << words.size() << std::endl;
+    // std::vector<std::string> words = common::utils::string_utils::split_words("Jesus is King!");
+    // std::cout << "Word count: " << words.size() << std::endl;
     std::cout << "MapReduce Worker" << std::endl;
     
     auto channel = grpc::CreateChannel(
@@ -20,9 +31,12 @@ int main() {
       grpc::InsecureChannelCredentials());
 
     mapreduce::worker::MasterClient client(channel);
-
-    bool ok = client.SendHeartbeat(1);
-
-    std::cout << "Heartbeat result: " << ok << std::endl;
+    
+    std::vector<std::thread> threads;
+    
+    threads.push_back(std::thread(heartbeat_thread, std::ref(client)));
+    
+    threads[0].join();
+    
     return 0;
 }
